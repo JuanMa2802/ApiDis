9d2d866403e6813370cdcb2e243d5a6f73fabd89
unknown <Juanma28o123@gmail.com>
2023-02-07 17:35:36 -0500

    despliegue2

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 #Ruta base
 @app.route("/")
 def home():
-    return "Api"
+    return "<h1 style='color:red;'>Api</h1>"
 
 @app.route("/Api/v3/Contratistas")
 def Contratistas():
