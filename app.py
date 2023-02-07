7063bec8cd13fa411dc2d2799e37dde1bf75f7f2
unknown <Juanma28o123@gmail.com>
2023-02-07 17:39:33 -0500

    despliegue con funcionalidad

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import subprocess
 
 
 app = Flask(__name__)
@@ -10,9 +10,10 @@ app = Flask(__name__)
 def home():
     return "<h1 style='color:red;'>Api</h1>"
 
-@app.route("/Api/v3/Contratistas")
-def Contratistas():
-    return "Contratistas"
+@app.route("/Api/v3/Contratistas/<string:Id>")
+def Contratistas(Id):
+    respuesta = subprocess.run(["python", "./scripts/login.py", Id], capture_output=True)
+    return respuesta.stdout
 
 if __name__ == "__main__":
     app.run()
