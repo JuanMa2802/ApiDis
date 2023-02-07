09a5f8eb731184bd6791818d363d9e66b200718c
unknown <Juanma28o123@gmail.com>
2023-02-07 17:47:49 -0500

    despliegue con selenium

--- scripts/login.py
+++ scripts/login.py
@@ -1,4 +1,16 @@
 import sys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import Select
 
 idPersona = int(sys.argv[1])
 print("Hola", idPersona)
