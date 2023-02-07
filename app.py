d84ae6a8ab31c7c4231983a3f32b0eda30c42b2f
unknown <Juanma28o123@gmail.com>
2023-02-07 16:00:24 -0500

    Segundo cambio

--- app.py
+++ app.py
@@ -7,12 +7,11 @@ from pathlib import Path
 from getpass import getuser
 
 
-aplicacion = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="templates")
 
 directoriobase = Path(__file__).resolve().parent.parent.parent
 usuario = getuser()
 directorioScript = "usuario"
-
 
 
 con = os.path.join(directoriobase)
@@ -21,17 +20,17 @@ con = os.path.join(directoriobase)
 # print(con)
 
 #Ruta base
-@aplicacion.route("/")
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@aplicacion.route("/Api/v2/Contratos/<string:Id>")
+@app.route("/Api/v2/Contratos/<string:Id>")
 def Contratos(Id):
     respuesta = subprocess.run(['python', r'.//scripts//login.py', Id])
     return respuesta.stdout
 
 
-@aplicacion.route("/Api/v2/U6uExY9bdDKQmmUTwes/contratistasArconsa/<string:Id>")
+@app.route("/Api/v2/U6uExY9bdDKQmmUTwes/contratistasArconsa/<string:Id>")
 def ContratistasCentralizados(Id):
     respuesta = subprocess.Popen([r'C:\Users\Juan Manuel Gaviria\Documents\ejecutables\Contratistas\dist\Contratistas\Contratistas.exe', Id])
     # respuesta = subprocess.call(['python', r"C:\Users\Juan Manuel Gaviria\Documents\DesarrollosX\Contratistas.py", Id])
@@ -39,7 +38,7 @@ def ContratistasCentralizados(Id):
     return "Ejecución en proceso..."
 
 
-@aplicacion.route("/Api/v2/U6uExY9bdDKQmmUTwes/CreacionProveedores/<string:Id>")
+@app.route("/Api/v2/U6uExY9bdDKQmmUTwes/CreacionProveedores/<string:Id>")
 def CreacionProveedores(Id):
     respuesta = subprocess.Popen([r'C:\Users\Juan Manuel Gaviria\Documents\ejecutables\CreacionProveedores\dist\CreacionProveedores\CreacionProveedores.exe', Id])
     # respuesta = subprocess.call(['python', r"C:\Users\Juan Manuel Gaviria\Documents\DesarrollosX\Contratistas.py", Id])
@@ -48,4 +47,4 @@ def CreacionProveedores(Id):
 
 
 if __name__ == "__main__":
-    aplicacion.run(debug=True, port=9500)
+    app.run(debug=True, port=9500)
