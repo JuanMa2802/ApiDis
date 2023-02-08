5f13af629385a9a866d800c230529b08aa593ef2
unknown <Juanma28o123@gmail.com>
2023-02-07 19:34:16 -0500

    Despliegue con funcionalidad mi ip

--- scripts/login.py
+++ scripts/login.py
@@ -37,8 +37,8 @@ opc.add_argument('--disable-dev-shm-usage')
 opc.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36")
 
 
-driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=opc)
-action = ActionChains(driver)
+# driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=opc)
+# action = ActionChains(driver)
 
 
 # driver.get('https://www1.sincoerp.com/SincoArconsa/V3/Marco/Login.aspx')
